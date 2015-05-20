9b100c5485d70b0b9c8c3fb0d8e35ddb08c9d27e
nsalerni <nick.salerni@gmail.com>
2015-05-20 16:56:40 -0400

    Updated BubbleSort.java
    Added a few comments to make the code more legible.

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -8,7 +8,7 @@
 public class BubbleSort
 {
     /*
-      Given an array, sort it using Bubble Sort.
+      Given an array, sort it using Bubble Sort. Time Complexity: O(n^2).
       Note: The algorithm works by comparing each item in the list with the item next to it, and swapping them if required.
             In other words, the largest element has bubbled to the top of the array. The algorithm repeats this process until
             it makes a pass all the way through the list without swapping any items.
@@ -20,6 +20,7 @@ public class BubbleSort
         {
             for (int j = 1; j <= i; j++)
             {
+                // If the element at (j-1) is greater than the element at (j), then the elements are out of order. Swap them.
                 if (array[j-1] > array[j])
                 {
                    int temp = array[j-1];
